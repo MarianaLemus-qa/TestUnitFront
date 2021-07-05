06a87eacb60121a52929ad0c95490e2892fbf22f
marianaLemus-qa <mariana.lemus@vecindario.com>
2021-07-05 17:00:09 +0200

    44

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-//test 33
+//test 34
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
